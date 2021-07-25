6c927ea72a601cf9714d38df0d1b60875c20c018
Zhongxin Hu <578981654@qq.com>
2021-07-25 18:26:09 -0400

    Update 170953060_180896440_a4.c

--- 170953060_180896440_a4.c
+++ 170953060_180896440_a4.c
@@ -1,1 +1,24 @@
 
+#include <stdio.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <string.h>
+#include <pthread.h>
+#include <sys/stat.h>
+#include <time.h>
+#include <semaphore.h>
+
+
+int read_File(char * f_name){
+  FILE *in_put = fopen(f_name,"r");
+  if(in_puit == NULL)
+   {
+      printf("Error! Can not open the input file\n");   
+      return -1;             
+   }
+
+  
+  
+  
+  
+}
