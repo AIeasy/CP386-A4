48e9a1da66a00e1889e9ce404613c9c0880ecdde
peng3060 <55161618+peng3060@users.noreply.github.com>
2021-07-25 19:37:00 -0400

    Create 170953060_180896440_a4.c

--- 170953060_180896440_a4.c
+++ 170953060_180896440_a4.c
@@ -8,22 +8,36 @@
 #include <time.h>
 #include <semaphore.h>
 
-
-int **read_File(char * f_name){
-	FILE *in_put = fopen(f_name,"r");
-	char matrix[5][4];
-	int i,j;
-	if(in_puit == NULL){
-		printf("Error! Can not open the input file\n");
-		return -1;
-	}
-	for(i=0; i<5; i++) {
-		for(j=0; j<4; j++) {
-		    fscanf(in_put," %c, ",&matrix[i][j]);
-		}
-	}
+int main(int argc, char *argv[]){
+	int n = 5,m = 4;//n number of processes, m number of resources types.
+        int max[n][m];
+        int Allocation[n][m];
+        int Available[m];
+        int Need[n][m]; //need = max - alloca
 	
-	return matrix;
+	//available = argv[]
+	//read file and generate max.
+	//safe check
+	//wait for request -> request
+}
+
+int safe_check(){
+	//
+	return 1;
+}
+
+
+
+int read_File(char * f_name){
+  FILE *in_put = fopen(f_name,"r");
+  if(in_puit == NULL)
+   {
+      printf("Error! Can not open the input file\n");   
+      return -1;             
+   }
+    int LINE_LENGTH = 128;
+    char *line = (char *)malloc(sizeof(char) * LINE_LENGTH);
+  
   
   
   
