f0265b28d73f991fa8d29753c3f7e109113a107f
Zhongxin Hu <578981654@qq.com>
2021-07-31 15:18:23 -0400

    Update 170953060_180896440_a4.c

--- 170953060_180896440_a4.c
+++ 170953060_180896440_a4.c
@@ -290,7 +290,7 @@ void *thread_run(void *thread){
   printf("        Needed:\n");//all the maxinum needed resources
 	  for (int y = 0;y<5;y++){
 	  	for(int n=0;n<4;n++){
-			printf("%s",customers[y].Need[n]);
+			printf("%c",customers[y].Need[n]);
 	  	}
 	  printf("\n");
   }
